8d2e30a69803b39ca24cdb331219d10b0212f53e
cosmin-s <n/a>
2015-05-09 00:54:46 +0300

    model

---
+++ src/main/java/home/cos/app/memento/model/Note.java
@@ -0,0 +1,40 @@
+package home.cos.app.memento.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Note {
+	
+	private String title;
+	private String content;
+	private Date date;
+	private List<String> tags;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public List<String> getTags() {
+		return tags;
+	}
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+	 
+	
+
+}
